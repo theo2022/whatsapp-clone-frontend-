9b912a96bedb6f4d0a894a740840be62ab8ca989
theo2022 <76963528+theo2022@users.noreply.github.com>
2021-01-05 16:23:35 +0530

    Add files via upload

---
+++ whatsapp leftside/giri.js
@@ -0,0 +1,30 @@
+function myclick8() {
+  var inputelement = document.getElementById("input");
+  inputelement.style.display = "block";
+  var inputelement1 = document.getElementById("sendbutton");
+  inputelement1.style.display = "block";
+
+  var element = document.createElement("div");
+  var node = document.createTextNode("giri");
+  element.appendChild(node);
+  var parent = document.getElementById("name");
+  parent.appendChild(element);
+  parent.style.display = "block";
+  var txt = "";
+  var arun = ["hi da","pubg podalama","waiting vanga","game start aaga poooguthu"];
+  arun.forEach(myfunction);
+
+  document.getElementById("demo").innerHTML = txt;
+  function myfunction(value) {
+    txt += value + "<br>" + "<br>";
+  }
+}
+function myFunction1() {
+  var x = document.forms["form"];
+  var text = "";
+  var i;
+  for (i = 0; i < x.length; i++) {
+    text += x.elements[i].value + "<br>" + "<br>";
+  }
+  document.getElementById("display").innerHTML += text;
+}
